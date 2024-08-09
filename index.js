a0f406a65aa93d6bae50cd326677babf49ba2d12
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-08-09 16:27:36 +0100

    changes made

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-//     '/blog/:resource/:id/show': '/:resource/:id'
-// }))
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blogs/:resource/:id/show': '/:resource/:id'
+}))
 
 server.use(router);
 
