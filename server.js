280ad3418f7e9425a289be2e4772ce817e9be661
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-08-09 06:41:51 +0100

    vercel.json created

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
-server.use('/api', router);
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+
+server.use(router);
 
 server.listen(3000, () => {
     console.log('JSON Server is running!');
 });
+
+module.exports = server
